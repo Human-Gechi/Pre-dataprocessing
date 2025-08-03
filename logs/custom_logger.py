6f6bf706f93f7f6239b553f442306b2c37fb83ed
RobaireTH <robaireth@gmail.com>
2025-08-03 07:26:52 +0100

    Update

--- logs/custom_logger.py
+++ logs/custom_logger.py
@@ -6,7 +6,7 @@ LOG_PATH = os.path.join(os.path.dirname(__file__), "apps.log")
 logging.basicConfig(
     level= logging.DEBUG,
     filename = LOG_PATH,
-    filemode = "w",
+    filemode = "a",
     format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt = "%Y-%m-%d %H:%M:%S"
 )
